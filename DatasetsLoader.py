a81759982ffa24cb50358fa0063dfded85e4023e
xiaoran <xiaoran2495897824@163.com>
2023-10-12 12:29:15 +0800

    datasetsloader init

--- DatasetsLoader.py
+++ DatasetsLoader.py
@@ -24,3 +24,103 @@ Datasets_list = ['Africa',
                  'YeastUn']
 
 
+
+
+
+def preprocess(dataset_name):
+    if dataset_name == 'Africa':
+        return load_Africa_data()
+    elif dataset_name == "BankNote":
+        return load_BankNote_data()
+    elif dataset_name == "CreditApproval":
+        return load_CreditApproval_data()
+    elif dataset_name == "CreditRisk":
+        return load_CreditRisk_data()
+    elif dataset_name == "CreditCard":
+        return load_CreditCard_data()
+    elif dataset_name == "Ecoli":
+        return load_Ecoli_data()
+    elif dataset_name == "FakeBills":
+        return load_FakeBills_data()
+    elif dataset_name == "LoanPrediction":
+        return load_LoanPrediction_data()
+    elif dataset_name == "PageBlock":
+        return load_PageBlock_data()
+    elif dataset_name == "PageBlockDel":
+        return load_PageBlockDel_data()
+    elif dataset_name == "PredictTerm":
+        return load_PredictTerm_data()
+    elif dataset_name == "SouthGermanCredit":
+        return load_SouthGermanCredit_data()
+    elif dataset_name == "Wine":
+        return load_Wine_data()
+    elif dataset_name == "WineRed":
+        return load_WineRed_data()
+    elif dataset_name == "Yeast":
+        return load_Yeast_data()
+    elif dataset_name == "FakeBills":
+        return load_YeastUn_data()
+
+
+def load_Africa_data():
+    pass
+
+
+def load_BankNote_data():
+    pass
+
+
+def load_CreditApproval_data():
+    pass
+
+
+def load_CreditRisk_data():
+    pass
+
+
+def load_CreditCard_data():
+    pass
+
+
+def load_Ecoli_data():
+    pass
+
+
+def load_FakeBills_data():
+    pass
+
+
+def load_LoanPrediction_data():
+    pass
+
+
+def load_PageBlock_data():
+    pass
+
+
+def load_PageBlockDel_data():
+    pass
+
+
+def load_PredictTerm_data():
+    pass
+
+
+def load_SouthGermanCredit_data():
+    pass
+
+
+def load_Wine_data():
+    pass
+
+
+def load_WineRed_data():
+    pass
+
+
+def load_Yeast_data():
+    pass
+
+
+def load_YeastUn_data():
+    pass
